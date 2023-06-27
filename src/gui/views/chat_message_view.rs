de148fdf5bbc7c485511df4e9e9700573d3e02e9
Kogasa <kogasapls@gmail.com>
2023-06-27 00:25:09 -0500

    add scuffed link parsing

--- src/gui/views/chat_message_view.rs
+++ src/gui/views/chat_message_view.rs
@@ -4,11 +4,27 @@ use dgg::dgg::models::emote::Emote;
 use eframe::egui;
 use eframe::egui::{Response, Rgba, Ui, Widget};
 use egui_extras::RetainedImage;
+use linkify::{Link, LinkFinder, LinkKind};
 use palette::{FromColor, Hsv, Srgb};
+use regex::{Captures, Regex};
 use serde::{Deserialize, Serialize};
+use serde_json::to_string;
+use std::cell::LazyCell;
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::process::id;
 use std::rc::Rc;
+use std::sync::LazyLock;
+
+// Regex for embed links
+static EMBED_REGEX: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(&format!(r"#(youtube|twitch|kick)/[a-zA-Z0-9]+")).unwrap());
+
+static LINK_FINDER: LazyLock<LinkFinder> = LazyLock::new(|| {
+    let mut link_finder = LinkFinder::new();
+    link_finder.kinds(&[linkify::LinkKind::Url]);
+    link_finder
+});
 
 /// A single chat message.
 #[derive(Clone)]
@@ -19,7 +35,7 @@ pub struct ChatMessageView {
     pub message: String,
     pub timestamp: String,
     pub flair_images: Vec<Rc<RetainedImage>>,
-    pub message_with_emotes: Vec<TextOrEmote>,
+    pub message_with_emotes: Vec<TextOrEmoteOrLink>,
 }
 
 impl Debug for ChatMessageView {
@@ -44,7 +60,7 @@ impl ChatMessageView {
         flair_images: Vec<Rc<RetainedImage>>,
         emote_images: &HashMap<String, Rc<RetainedImage>>,
     ) -> Self {
-        let message_with_emotes = Self::parse_message_with_emotes(&message, emote_images);
+        let message_with_emotes = Self::parse_message(&message, emote_images);
 
         Self {
             username,
@@ -57,10 +73,60 @@ impl ChatMessageView {
         }
     }
 
-    fn parse_message_with_emotes(
+    fn parse_message(
         message: &str,
         emotes: &HashMap<String, Rc<RetainedImage>>,
-    ) -> Vec<TextOrEmote> {
+    ) -> Vec<TextOrEmoteOrLink> {
+        let mut last_index = 0;
+
+        let links: Vec<_> = LINK_FINDER.links(message).collect();
+
+        let mut add_word_or_emote = |tokens: &mut Vec<TextOrEmoteOrLink>, word: &str| {
+            if let Some(captures) = EMBED_REGEX.captures(word) {
+                let platform = captures.get(1).unwrap().as_str().to_ascii_lowercase();
+                let id = captures.get(2).unwrap().as_str().to_ascii_lowercase();
+                tokens.push(TextOrEmoteOrLink::EmbedLink(EmbedLink::new(
+                    word,
+                    platform.as_str(),
+                    id.as_str(),
+                )));
+            } else if let Some(emote) = emotes.get(word) {
+                tokens.push(TextOrEmoteOrLink::Emote(emote.clone()));
+            } else {
+                tokens.push(TextOrEmoteOrLink::Text(word.to_string()));
+            }
+        };
+
+        let mut tokens = Vec::new();
+        for link in links {
+            // Process the text before the link
+            if link.start() > last_index {
+                let substr = &message[last_index..link.start()];
+                for word in substr.split_whitespace() {
+                    add_word_or_emote(&mut tokens, word);
+                }
+            }
+
+            // Process the link
+            tokens.push(TextOrEmoteOrLink::Link(link.as_str().to_string()));
+            last_index = link.end();
+        }
+
+        // Process the remaining text after the last link
+        if last_index < message.len() {
+            let substr = &message[last_index..];
+            for word in substr.split_whitespace() {
+                add_word_or_emote(&mut tokens, word);
+            }
+        }
+
+        tokens
+    }
+
+    fn parse_message_with_emotes_no_links(
+        message: &str,
+        emotes: &HashMap<String, Rc<RetainedImage>>,
+    ) -> Vec<TextOrEmoteOrLink> {
         let mut message_with_emotes = Vec::new();
         let mut push_text_or_emote = |current_word: String| {
             if current_word.is_empty() {
@@ -68,9 +134,9 @@ impl ChatMessageView {
             }
 
             if let Some(emote) = emotes.get(&current_word) {
-                message_with_emotes.push(TextOrEmote::Emote(emote.clone()));
+                message_with_emotes.push(TextOrEmoteOrLink::Emote(emote.clone()));
             } else {
-                message_with_emotes.push(TextOrEmote::Text(current_word.clone()));
+                message_with_emotes.push(TextOrEmoteOrLink::Text(current_word.clone()));
             }
         };
 
@@ -142,18 +208,65 @@ impl View for ChatMessageView {
 }
 
 #[derive(Clone)]
-pub enum TextOrEmote {
+pub enum TextOrEmoteOrLink {
     Text(String),
     Emote(Rc<RetainedImage>),
+    Link(String),
+    EmbedLink(EmbedLink),
 }
 
-impl View for TextOrEmote {
+#[derive(Debug, Clone)]
+pub enum EmbedLinkKind {
+    YouTube,
+    Twitch,
+    Kick,
+}
+
+#[derive(Debug, Clone)]
+pub struct EmbedLink {
+    pub platform: EmbedLinkKind,
+    pub id: String,
+    pub original: String,
+}
+
+impl EmbedLink {
+    pub fn new(original: &str, platform: &str, id: &str) -> Self {
+        let platform = match platform {
+            "youtube" => EmbedLinkKind::YouTube,
+            "twitch" => EmbedLinkKind::Twitch,
+            "kick" => EmbedLinkKind::Kick,
+            _ => panic!("Unknown embed platform: {}", platform),
+        };
+
+        Self {
+            platform,
+            id: id.to_string(),
+            original: original.to_string(),
+        }
+    }
+
+    pub fn url(&self) -> String {
+        match self.platform {
+            EmbedLinkKind::YouTube => format!("https://www.youtube.com/watch?v={}", self.id),
+            EmbedLinkKind::Twitch => format!("https://www.twitch.tv/{}", self.id),
+            EmbedLinkKind::Kick => format!("https://kick.tv/{}", self.id),
+        }
+    }
+
+    pub fn value(&self) -> &str {
+        self.original.as_str()
+    }
+}
+
+impl View for TextOrEmoteOrLink {
     fn show(&self, ui: &mut Ui) -> Response {
         match self {
-            TextOrEmote::Text(text) => ui.label(text),
-            TextOrEmote::Emote(image) => {
+            TextOrEmoteOrLink::Text(text) => ui.label(text),
+            TextOrEmoteOrLink::Emote(image) => {
                 ui.image(image.texture_id(ui.ctx()), egui::Vec2::new(16.0, 16.0))
             }
+            TextOrEmoteOrLink::Link(link) => ui.hyperlink_to(link, link),
+            TextOrEmoteOrLink::EmbedLink(link) => ui.hyperlink_to(link.value(), link.url()),
         }
     }
 }
